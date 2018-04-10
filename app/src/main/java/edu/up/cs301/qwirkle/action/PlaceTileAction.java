4b9348917c3a450dd946230b1893ce6f1ac27240
Quacheronies <quach19@Up.edu>
2018-04-10 11:15:47 -0700

    Place Tile Action Changes

--- app/src/main/java/edu/up/cs301/qwirkle/action/PlaceTileAction.java
+++ app/src/main/java/edu/up/cs301/qwirkle/action/PlaceTileAction.java
@@ -2,6 +2,8 @@ package edu.up.cs301.qwirkle.action;
 
 import edu.up.cs301.game.GamePlayer;
 import edu.up.cs301.game.actionMsg.GameAction;
+import edu.up.cs301.qwirkle.tile.QwirkleTile;
+import edu.up.cs301.qwirkle.ui.MainBoard;
 
 /**
  * Class: PlaceTileAction
@@ -12,7 +14,25 @@ import edu.up.cs301.game.actionMsg.GameAction;
  * @version April 10, 2018
  */
 public class PlaceTileAction extends GameAction {
-    public PlaceTileAction(GamePlayer player) {
+    //instance variables: tile position and player hands
+    private QwirkleTile playerHands[][];
+    private int xPos;
+    private int yPos;
+
+    /*public PlaceTileAction(GamePlayer player, QwirkleTile playerHands, int xPos, int yPos) {
         super(player);
     }
+
+    public QwirkleTile[][] getPlayerHands() {
+        return playerHands;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }*/
 }
+
